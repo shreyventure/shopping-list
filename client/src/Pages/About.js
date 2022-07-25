c132c20b0030dfcf23ae783aedf8caf167243d67
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2022-07-25 13:19:56 +0530

    Add files via upload
    Added an about page

---
+++ client/src/Pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div><h1>About</h1>
+    <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum orci in ante cursus euismod. Cras volutpat malesuada aliquam. Morbi condimentum mollis risus nec malesuada. Nam feugiat scelerisque rutrum. Maecenas lobortis posuere ligula, ac volutpat nisi laoreet sed. Ut nisi sem, vehicula et orci ac, commodo scelerisque nibh. Cras ut urna lorem. Sed vitae euismod metus. Suspendisse elementum tellus sit amet elit accumsan sagittis eget sed velit. Maecenas nisi erat, viverra non faucibus ac, gravida at ex.
+
+Ut faucibus metus et lectus aliquam tincidunt. Fusce sit amet ante at libero bibendum volutpat vitae sed ex. Nunc sed ipsum ut lectus maximus luctus in et mi. Integer iaculis libero id sem imperdiet, sed dignissim nulla luctus. Nulla at dui et diam fringilla tincidunt. Nunc id maximus libero. Curabitur varius libero quis elit volutpat, eget fermentum lacus accumsan. Nam nibh metus, dignissim in consectetur at, convallis non lorem. Vivamus in malesuada dui, quis imperdiet nulla.
+
+Nullam ac erat nec metus laoreet interdum. Aliquam odio magna, rutrum ut tortor a, interdum ultricies dolor. Praesent egestas nunc vel sapien ornare, quis pharetra ligula tincidunt. Etiam sit amet massa lacinia, pharetra nulla vitae, tempus ante. Proin dictum viverra ex sollicitudin convallis. Nam efficitur nunc nec felis varius, vel convallis neque commodo. Etiam aliquam nulla lacus, nec feugiat justo aliquam vitae.
+
+Phasellus consectetur consectetur enim, vel pellentesque quam tempus vel. Quisque augue mi, eleifend sed dignissim eu, pellentesque et diam. Sed semper molestie nulla imperdiet aliquam. Cras efficitur at quam eu consectetur. Sed sagittis ex ante. Duis condimentum auctor ligula, a auctor quam aliquet ut. Aliquam aliquam ac lectus quis iaculis.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id auctor orci. Suspendisse volutpat semper massa vel porttitor. Cras luctus tincidunt luctus. Quisque sollicitudin vel lorem eget accumsan. Integer convallis facilisis turpis, vitae ullamcorper nunc pretium quis. Etiam nisi lacus, varius vel malesuada id, porttitor eget lacus. Pellentesque et tempor diam. Ut bibendum, ipsum at accumsan condimentum, tortor erat mollis mauris, ut convallis dolor erat in orci. Cras sollicitudin sagittis massa eu fermentum. Suspendisse tempor nisi non purus dapibus, quis sollicitudin metus tincidunt. Etiam fermentum viverra tortor quis aliquet. Etiam in risus ante. Donec pharetra tincidunt diam, convallis laoreet ante ornare et. Phasellus ullamcorper, est id facilisis rutrum, ligula lacus elementum sapien, et sodales augue libero at eros.
+</p>
+    </div>
+  )
+}
+export default About
