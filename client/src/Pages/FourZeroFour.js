3759677de6ed831961fee649db3f56467a1890e9
Shreyas Shrawage <shreyas.shrawage2000@gmail.com>
2022-07-28 12:00:31 +0530

    Added a 404 png image

--- client/src/Pages/FourZeroFour.js
+++ client/src/Pages/FourZeroFour.js
@@ -1,9 +1,10 @@
 import React from "react";
-import error404 from "../Images/error404.jpg";
+import error404 from "../Images/error404.png";
 const FourZeroFour = () => {
   return (
-    <div className="w-100 d-flex justify-content-center align-items-center border">
+    <div className="w-100 d-flex justify-content-center align-items-center flex-column">
       <img src={error404} alt="" width={"70%"} />
+      <p className="lead">Page not found...</p>
     </div>
   );
 };
